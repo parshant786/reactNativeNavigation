4fcc6d66e5f688c0435e46ba27d613fe1a105faa
Parshant Jain <parshant.jain@letx.co>
2024-05-27 09:20:15 +0530

    add changes

---
+++ screens/CategoryScreen.js
@@ -0,0 +1,10 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+
+export const CategoryScreen = () => {
+  return (
+   <View>
+    <View><FlatList /></View>
+   </View>
+  )
+}
